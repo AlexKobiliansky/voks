a57fb753d6c5da9535710fb6f3a729414564789a
alex <alex_kobilianskiy@bigmir.net>
2019-03-10 12:59:37 +0200

    header complete

--- app/js/common.js
+++ app/js/common.js
@@ -45,6 +45,98 @@ $(function() {
      * end mobile-mnu customization
      */
 
+
+    $('img.svg').each(function(){
+        var $img = jQuery(this);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
+
+        jQuery.get(imgURL, function(data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = jQuery(data).find('svg');
+
+            // Add replaced image's ID to the new SVG
+            if(typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if(typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass+' replaced-svg');
+            }
+
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
+
+            // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+            if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+                $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+            }
+
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+        }, 'xml');
+    });
+
+
+
+
+    $('.intro-slider').owlCarousel({
+        loop:true,
+        nav:false,
+        items: 1,
+        thumbs: true,
+        thumbsPrerendered: true,
+        thumbItemClass: 'nav-item',
+        animateIn: "fadeIn",
+        animateOut: "fadeOut",
+        responsive: {
+            0: {
+                dots: true,
+                margin: 15,
+                mouseDrag: true
+            },
+            768: {
+                dots: false,
+                margin: 0,
+                mouseDrag: false
+            }
+        }
+    });
+
+    function heightses() {
+        if ($(window).width()>480) {
+
+        }
+
+        $('.nav-item-title').matchHeight();
+        $('.intro-slide').matchHeight();
+    }
+
+    $(window).resize(function() {
+        heightses();
+    });
+
+    heightses();
+
+    /**
+     * FORMS
+     */
+
+    var uPhone = $('.user-phone');
+    uPhone.mask("+7 (999) 999-99-99",{autoclear: false});
+
+    uPhone.on('click', function (ele) {
+        var needelem = ele.target || event.srcElement;
+        needelem.setSelectionRange(4,4);
+        needelem.focus();
+    });
+
+    $.validate({
+        form : '.contact-form',
+        scrollToTopOnError: false
+    });
+
     //E-mail Ajax Send
     $("form").submit(function() { //Change
         var th = $(this);
@@ -58,4 +150,7 @@ $(function() {
         });
         return false;
     });
+    /**
+     * FORMS end
+     */
 });
